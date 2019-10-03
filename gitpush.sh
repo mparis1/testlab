c7e426d9eea889178944e82b8bce909aac1c6827
mparis1 <mvirga01@gmail.com>
2019-10-03 02:53:52 +0000

    Test2

--- gitpush.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-curl -k --header "Authorization: ZyXLWFdK2OCoWqGVi6XnPU0bbxmbmaw1lDMbpt4i" --header "Accept: application/json" --header "Content-Type: application/json" https://10.93.8.35/attributes/text/download/md6/ > test30.txt
-git add .
-git commit -m "Test2"
-git pull
-git push origin master
-
-
-
